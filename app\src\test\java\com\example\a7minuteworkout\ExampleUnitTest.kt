1f5faa8a5c04ba61ef2ab87b3d8673b5c5add458
Abhishek Kumar <ak8384031@gmail.com>
2024-04-05 23:08:04 +0530

    Add documentation for ExampleUnitTest.kt

---
+++ app\src\test\java\com\example\a7minuteworkout\ExampleUnitTest.kt
@@ -0,0 +1,28 @@
+**Folder Name:** com.example.a7minuteworkout
+
+**File Name:** ExampleUnitTest.java
+
+**Line by Line Documented Code:**
+
+```java
+package com.example.a7minuteworkout;
+
+import org.junit.Test;
+
+import org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() {
+        // This is a test method that is used to test the correctness of addition.
+        // This test asserts that the sum of 2 and 2 is equal to 4.
+        assertEquals(4, 2 + 2);
+    }
+}
+```
